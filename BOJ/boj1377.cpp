4c85b0ec023ceded7f133bd6043d027762fde181
Youth <boysoeng@naver.com>
2023-05-25 15:34:00 +0900

    set: test commit2

---
+++ BOJ/boj1377.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int A[505050];
+
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char c;
+    cin>>c;
+    int tmp = c-'0';
+    cout<<tmp;
+    return 0;
+    
+
+    return 0;
+}
